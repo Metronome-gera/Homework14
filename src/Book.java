5c6916f69994a16acca794d627cd894e9cc7e459
Роман <roninmtn@gmail.com>
2025-09-02 21:10:33 +0400

    3 часа томил свинину

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,31 @@
-public class Book {
+class Author {
+    private String firstName;
+    public  Author (String firstName) {
+        this.firstName = firstName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
+public class Book {
+    private String title;
+    private int ageOfRelyease;
+
+    public Book(String title, int ageOfRelyease) {
+        this.title = title;
+        this.ageOfRelyease = ageOfRelyease;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public int getAgeOfRelyease() {
+        return ageOfRelyease;
+    }
+    public void setAgeOfRelease(int ageOfRelyease) {
+        this.ageOfRelyease = ageOfRelyease;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Book bushido = new Book("bushido", 1900);
+        bushido.setAgeOfRelease(1990);
+        Author bushido1 = new Author("Inoji Nitobe");
+        Book sauces = new Book("Sauces", 2021);
+        Author sauces1 = new Author("Tom Feller");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+         System.out.println(" = " +bushido.getAgeOfRelyease() );
     }
 }
